d6a42d9203eafc03b02dd7b54d8b0abd9539f0a1
ludoviclefevre <contact@ludoviclefevre.fr>
2015-04-08 18:39:04 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var merge = require('utils-merge');
+var pathFn = require('path');
+
+var config = hexo.config.sitemap = merge({
+    path: 'sitemap.xml'
+}, hexo.config.sitemap);
+
+if (!pathFn.extname(config.path)) {
+    config.path += '.xml';
+}
+
+hexo.extend.generator.register('sitemap', require('./lib/generator'));
