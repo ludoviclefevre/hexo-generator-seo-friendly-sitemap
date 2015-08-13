e573215933d3e82189199c79f4018232cb7d4ed0
ludoviclefevre <contact@ludoviclefevre.fr>
2015-08-13 18:30:10 +0200

    Fixing tests

---
+++ test/xmlValidator.js
@@ -0,0 +1,60 @@
+(function () {
+    'use strict';
+
+    var moment = require('moment'),
+        path = require('path'),
+        _ = require('lodash'),
+        ejs = require('ejs'),
+        chai = require('chai'),
+        assert = chai.assert,
+        DOMParser = require('xmldom').DOMParser,
+        Promise = require('bluebird'),
+        fs = Promise.promisifyAll(require('fs')),
+        viewsDirPath = 'views',
+        views = [{
+            'type': 'categories',
+            'path': path.join(viewsDirPath, 'category-sitemap.xml')
+        }];
+
+    describe('SEO-friendly sitemap generator: ', function () {
+        it('should generate a valid category-sitemap.xml file.', function () {
+            return getCompiledContent('categories')
+                .then(xmlValidator);
+        });
+    });
+
+    var getCompiledContent = function (type) {
+        var filePath = _(views).find('type', type);
+        assert.isNotNull(path, 'We must have a path for view ' + type);
+
+        var boundData = {
+            config: {
+                'url': 'http://yoursite.com'
+            },
+            items: [
+                {
+                    'permalink': 'http://yoursite.com/categories/Cat1/',
+                    'updated': moment().toDate()
+                }
+            ]
+        };
+
+        return fs.readFileAsync(filePath)
+            .then(function (content) {
+                var compiledTemplate = ejs.compile(content),
+                    xml = compiledTemplate(boundData);
+                return xml;
+            });
+    };
+
+
+    var xmlValidator = function (xml) {
+        new DOMParser({
+            locator: {},
+            errorHandler: function (level, msg) {
+                Promise.reject(msg);
+            }
+        }).parseFromString(xml, 'text/xml');
+        Promise.resolve();
+    };
+})();
